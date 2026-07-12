c1a3cc4facf17b0afb202858ab75052b8f3eecb6
Ambra Digital Solution <admin@ambradigitalsolution.com>
2026-07-12 16:38:48 +0700

    Match Sriguna format: clean bottom section with Baca juga only

--- single.php
+++ single.php
@@ -74,32 +74,26 @@ get_header();
                                     ?>
                                 </div>
                                 
-                                <?php if ( has_tag() ) : ?>
-                                    <div class="single-post-tags">
-                                        <?php the_tags( '', '', '' ); ?>
-                                    </div>
-                                <?php endif; ?>
-
                                 <!-- Baca Juga / Related Posts -->
-                                <?php
-                                $categories = get_the_category();
-                                if ( $categories ) :
-                                    $category_ids = array();
-                                    foreach ( $categories as $category ) {
-                                        $category_ids[] = $category->term_id;
-                                    }
-                                    $related_args = array(
-                                        'category__in'   => $category_ids,
-                                        'post__not_in'   => array( get_the_ID() ),
-                                        'posts_per_page' => 3,
-                                        'orderby'        => 'rand',
-                                        'post_status'    => 'publish',
-                                    );
-                                    $related_query = new WP_Query( $related_args );
-                                    if ( $related_query->have_posts() ) :
-                                ?>
                                 <div class="related-posts-section">
                                     <h3 class="related-posts-title">Baca juga:</h3>
+                                    <?php
+                                    $categories = get_the_category();
+                                    if ( $categories ) :
+                                        $category_ids = array();
+                                        foreach ( $categories as $category ) {
+                                            $category_ids[] = $category->term_id;
+                                        }
+                                        $related_args = array(
+                                            'category__in'   => $category_ids,
+                                            'post__not_in'   => array( get_the_ID() ),
+                                            'posts_per_page' => 3,
+                                            'orderby'        => 'rand',
+                                            'post_status'    => 'publish',
+                                        );
+                                        $related_query = new WP_Query( $related_args );
+                                        if ( $related_query->have_posts() ) :
+                                    ?>
                                     <div class="related-posts-grid">
                                         <?php while ( $related_query->have_posts() ) : $related_query->the_post(); ?>
                                         <a href="<?php the_permalink(); ?>" class="related-post-card">
@@ -117,19 +111,12 @@ get_header();
                                         </a>
                                         <?php endwhile; ?>
                                     </div>
-                                </div>
-                                <?php
+                                    <?php
+                                        endif;
+                                        wp_reset_postdata();
                                     endif;
-                                    wp_reset_postdata();
-                                endif;
-                                ?>
-                                
-                                <?php
-                                // If comments are open or we have at least one comment, load up the comment template.
-                                if ( comments_open() || get_comments_number() ) :
-                                    comments_template();
-                                endif;
-                                ?>
+                                    ?>
+                                </div>
                             </article>
                         <?php endwhile; // End of the loop. ?>
                     </div>
